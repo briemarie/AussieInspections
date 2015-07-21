9045b875ec8a9a62ed34f81c3aeb8263737a675b
briemarie <thebriemarie@gmail.com>
2015-07-21 16:11:36 -0700

    Add Queensland report

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,22 @@ get '/Victoria' do
   erb :vic
 end
 
+get '/ACT' do
+  erb :act
+end
+
+get '/Queensland' do
+  erb :queens
+end
+
+get '/South' do
+  erb :south
+end
+
+get '/West' do
+  erb :west
+end
+
 
 
 
