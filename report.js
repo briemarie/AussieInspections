1178115b90df21f9ac71b42892fc6cc53886998d
briemarie <thebriemarie@gmail.com>
2015-07-21 15:14:05 -0700

    Remove extra padding on bottom for better styling

--- report.js
+++ report.js
@@ -1,1 +1,36 @@
-var report = {"signatories":{},"data":{"assetDetails":[{"class":"unit","text":"010"},{"class":"building","text":"Building 1"},{"class":"floor-plan","text":"2 Bed 1  Bath"},{"class":"address","text":"Oakland\nCA"}],"bodySections":[],"config":{"locale":"en-US","timeZoneOffset":-25200},"createdAt":"2015-07-06T16:27:40.490Z","footerFields":[{"title":"General Comments"}],"headerFields":[],"inspectedAt":"2015-07-02T12:19:06.361-07:00","inspection":{"endedAt":"2015-07-06T09:27:36.684-07:00","id":"yAuKjB-A-QL","startedAt":"2015-07-02T12:19:06.361-07:00"},"template":"standard","title":"Periodic inspection"}}
+var report = {
+    "signatories": {},
+    "data": {
+        "assetDetails": [{
+            "class": "unit",
+            "text": "010"
+        }, {
+            "class": "building",
+            "text": "Building 1"
+        }, {
+            "class": "floor-plan",
+            "text": "2 Bed 1  Bath"
+        }, {
+            "class": "address",
+            "text": "Oakland\nCA"
+        }],
+        "bodySections": [],
+        "config": {
+            "locale": "en-US",
+            "timeZoneOffset": -25200
+        },
+        "createdAt": "2015-07-06T16:27:40.490Z",
+        "footerFields": [{
+            "title": "General Comments"
+        }],
+        "headerFields": [],
+        "inspectedAt": "2015-07-02T12:19:06.361-07:00",
+        "inspection": {
+            "endedAt": "2015-07-06T09:27:36.684-07:00",
+            "id": "yAuKjB-A-QL",
+            "startedAt": "2015-07-02T12:19:06.361-07:00"
+        },
+        "template": "standard",
+        "title": "Periodic inspection"
+    }
+}
